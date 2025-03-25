7b383303e922930d74b7db47eca7309fd00359c6
PedroRafael04 <pedrorafaelofficial@gmail.com>
2025-03-25 20:17:29 -0300

    Update Testes.java

--- Trabalho01_Parte03/src/main/java/problema3/Testes.java
+++ Trabalho01_Parte03/src/main/java/problema3/Testes.java
@@ -1,91 +1,85 @@
 package problema3;
 
+import br.furb.analise.algoritmos.LampadaPhellipes;
+import org.junit.Test;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+public class TesteTest {
 
-import br.furb.analise.algoritmos.PersianaNatLight;
+    @Test
+    public void testPersianaSolariusAdapter() throws Exception {
+        Persiana persiana = new PersianaSolariusAdaptador();
 
-public class Testes {
-	
-	 @Test
-	    public void testPersianaSolariusAdapter() throws Exception {
-	        Persiana persiana = new PersianaSolariusAdaptador();
 
-	        
-	        persiana.abrir();
-	        assertTrue(persiana instanceof Persiana);
+        persiana.abrir();
+        assertTrue(persiana instanceof Persiana);
 
-	        
-	        persiana.fechar();
-	        assertTrue(persiana instanceof Persiana);
-	    }
 
-	    @Test
-	    public void testPersianaNatLightAdapter() throws Exception {
-	    	Persiana persiana = new PersianaNatLightAdaptador();
+        persiana.fechar();
+        assertTrue(persiana instanceof Persiana);
+    }
 
-	        
-	        persiana.abrir();
-	        assertTrue(persiana.estaPalhetasAbertas());
+    @Test
+    public void testPersianaNatLightAdapter() throws Exception {
+        Persiana persiana = new PersianaNatLightAdaptador();
 
-	        
-	        persiana.fechar();
-	        assertFalse(persiana.estaAberta());
-	    }
-	    
-	    @Test
-	    public void testArVentoBaumnAdapter() {
-	        ArCondicionado ar = new ArCondicionadoVentoBaumnAdaptador();
-	        
-	        ar.ligar();
-	        assertTrue(ar instanceof ArCondicionado);
-	        
-	        ar.definirTemperatura(20);
-	        ar.desligar();
-	        assertTrue(ar instanceof ArCondicionado);
-	    }
 
-	    @Test
-	    public void testArGellaKazaAdapter() {
-	        ArCondicionado ar = new ArCondicionadoGellaKazaAdaptador();
-	        
-	        ar.ligar();
-	        assertTrue(ar instanceof ArCondicionado);
-	        
-	        ar.aumentarTemperatura();
-	        ar.diminuirTemperatura();
-	        
-	        ar.desligar();
-	        assertTrue(ar instanceof ArCondicionado);
-	    }
-	    
-	    @Test
-	    public void testLampadaShoyouMiAdapter() {
-	        Lampada lampada = new LampadaShoyouMiAdaptador();
-	        
-	        lampada.ligar();
-	        assertTrue(lampada instanceof Lampada);
-	        
-	        lampada.desligar();
-	        assertTrue(lampada instanceof Lampada);
-	    }
+        persiana.abrir();
+        assertTrue(persiana.estaPalhetasAbertas());
 
-	    @Test
-	    public void testLampadaPhelippesAdapter() {
-	        Lampada lampada = new LampadaPhellipesAdaptador();
-	        
-	        lampada.ligar();
-	        assertTrue(lampada instanceof Lampada);
-	        
-	        lampada.desligar();
-	        assertTrue(lampada instanceof Lampada);
-	    }
-	    
-	    
-	    
-	    
-	    
+
+        persiana.fechar();
+        assertTrue(persiana.estaAberta());
+    }
+
+    @Test
+    public void testArVentoBaumnAdapter() {
+        ArCondicionado ar = new ArCondicionadoVentoBaumnAdaptador();
+
+        ar.ligar();
+        assertTrue(ar instanceof ArCondicionado);
+
+        ar.definirTemperatura(20);
+        ar.desligar();
+        assertTrue(ar instanceof ArCondicionado);
+    }
+
+    @Test
+    public void testArGellaKazaAdapter() {
+        ArCondicionado ar = new ArCondicionadoGellaKazaAdaptador();
+
+        ar.ligar();
+        assertTrue(ar instanceof ArCondicionado);
+
+        ar.aumentarTemperatura();
+        ar.diminuirTemperatura();
+
+        ar.desligar();
+        assertTrue(ar instanceof ArCondicionado);
+    }
+
+    @Test
+    public void testLampadaShoyouMiAdapter() {
+        Lampada lampada = new LampadaShoyouMiAdaptador();
+
+        lampada.ligar();
+        assertTrue(lampada instanceof Lampada);
+
+        lampada.desligar();
+        assertTrue(lampada instanceof Lampada);
+    }
+
+    @Test
+    public void testLampadaPhelippesAdapter() {
+        Lampada lampada = new LampadaPhellipesAdaptador();
+
+        lampada.ligar();
+        assertTrue(lampada instanceof Lampada);
+
+        lampada.desligar();
+        assertTrue(lampada instanceof Lampada);
+    }
 
 }
